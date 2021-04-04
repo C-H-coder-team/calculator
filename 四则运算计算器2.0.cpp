2503dc87bd58f49858bd2b724e3d0d3f6d33e0dd
hacker-Fanjun <78690857+hacker-Fanjun@users.noreply.github.com>
2021-04-04 19:55:44 +0800

    Create 四则运算计算器2.0.cpp

---
+++ 四则运算计算器2.0.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<fstream>
+#include<algorithm>
+   
+using namespace std;
+char f;
+int s1,s2;
+int main()
+{
+cin>>s1>>f>>s2;
+if(f=='/' and s2==0)cout<<"NO";
+   
+if(f=='/' and s2!=0)cout<<s1<<'/'<<s2<<'='<<s1/s2;
+   
+if(f=='-' )cout<<s1<<'-'<<s2<<'='<<s1-s2;
+   
+if(f=='*')cout<<s1<<'*'<<s2<<'='<<s1*s2;
+   
+if(f=='+' )cout<<s1<<'+'<<s2<<'='<<s1+s2;
+   
+       
+   
+    return 0;
+}
+//upload by fanjun
